90ba98c5f5bdec227159b9fa1b097ee382b7f43f
oriphone <oriol@MacBook-Air.local>
2016-03-07 20:56:30 +0100

    optimizando raycast
    cambios de raycast a spherecast para aobtimizar la mecánica y evitar
    colisiones al instanciar

--- Assets/Scripts/Cubos.cs
+++ Assets/Scripts/Cubos.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class Cubos : MonoBehaviour
 {
@@ -8,7 +9,7 @@ public class Cubos : MonoBehaviour
     public float minTiempoEntreCubos = 1.0f, maxTiempoEntreCubos = 1.5f;
     public float minX = -24.0f, maxX = 24.0f;
     public float minZ = -11.0f, maxZ = 11.0f;
-    public float y = 0.5f;
+    public float y = 1.0f;
 	float tiempo = 60.0f;
 
     private bool lanzadorCubos = true;
@@ -17,8 +18,20 @@ public class Cubos : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+		gameObject.transform.position = new Vector3 (Random.Range (minX, maxX), 25, Random.Range (minZ, maxZ));
         StartCoroutine(CubosEnJuego());
 		StartCoroutine(CuentaAtras());
+	}
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.R)) {
+			SceneManager.LoadScene ("CatchTheColor");
+		}
+		if (GameManager.Blanco == GameManager.full) {
+			GameManager.enableBlanco = false;
+		} else if (GameManager.Blanco < GameManager.full){
+			GameManager.enableBlanco = true;
+		}
 	}
 	IEnumerator CuentaAtras()
 	{
@@ -32,34 +45,38 @@ public class Cubos : MonoBehaviour
     // Update is called once per frame
 
     IEnumerator CubosEnJuego()
-    {
-        // Initial delay
-        yield return new WaitForSeconds(2.0f);
+	{
+		// Initial delay
+		yield return new WaitForSeconds (2.0f);
 
-        while (lanzadorCubos)
-        {
-
-			if (GameManager.enableBlanco) 
-			{
-				GameObject cubo = (GameObject)Instantiate (cubos [Random.Range (0, cubos.Length)]);
-				cubo.transform.position = new Vector3 (Random.Range (minX, maxX), y, Random.Range (minZ, maxZ));
-				cubo.transform.rotation = Random.rotation;
-				rigidbody = cubo.GetComponent<Rigidbody> ();
-				GameManager.cubosLanzados++;
-			} 
-			else 
-			{
-				GameObject cubo = (GameObject)Instantiate (cubos [Random.Range (0, 5)]);
-				cubo.transform.position = new Vector3 (Random.Range (minX, maxX), y, Random.Range (minZ, maxZ));
-				cubo.transform.rotation = Random.rotation;
-				rigidbody = cubo.GetComponent<Rigidbody> ();
-				GameManager.cubosLanzados++;
+		while (lanzadorCubos) {
+			RaycastHit hit;
+			Ray rayo = new Ray (transform.position, Vector3.down);
+			if (Physics.SphereCast (rayo,1.0f, out hit, 30.0f)) {
+				if (hit.collider.tag == "Respawn") {
+					if (GameManager.enableBlanco) {
+						print ("respawn!!!!!");
+						GameObject cubo = (GameObject)Instantiate (cubos [Random.Range (0, cubos.Length)]);
+						cubo.transform.position = new Vector3 (transform.position.x, y, transform.position.z);
+						cubo.transform.rotation = Random.rotation;
+						rigidbody = cubo.GetComponent<Rigidbody> ();
+						GameManager.cubosLanzados++;
+						gameObject.transform.position = new Vector3 (Random.Range (minX, maxX), 25, Random.Range (minZ, maxZ));
+					} else {
+						print ("respawn!!!!!");
+						GameObject cubo = (GameObject)Instantiate (cubos [Random.Range (0, 5)]);
+						cubo.transform.position = new Vector3 (transform.position.x, y, transform.position.z);
+						cubo.transform.rotation = Random.rotation;
+						rigidbody = cubo.GetComponent<Rigidbody> ();
+						GameManager.cubosLanzados++;
+						gameObject.transform.position = new Vector3 (Random.Range (minX, maxX), 25, Random.Range (minZ, maxZ));
+					}
+				} else {
+					gameObject.transform.position = new Vector3 (Random.Range (minX, maxX), 25, Random.Range (minZ, maxZ));
+					print ("not respawn.");
+				}
 			}
-       
-            yield return new WaitForSeconds(Random.Range(minTiempoEntreCubos, maxTiempoEntreCubos));
-
-        }
-
-    }
-
+			yield return new WaitForSeconds (Random.Range (minTiempoEntreCubos, maxTiempoEntreCubos));
+		}
+	}
 }
